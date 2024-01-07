d0311dc63124316e3f30eebee92e06035589fe36
Lakshay <lakshaykhokhar2003@gmail.com>
2024-01-07 20:55:56 +0530

    fix: minor bug fix

--- backend.js
+++ backend.js
@@ -1,5 +1,4 @@
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '.env')});
+require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express();
